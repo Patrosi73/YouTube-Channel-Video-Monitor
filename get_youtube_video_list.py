e9c4ee51e9a18b983e21a113bf89771776d44d1d
Patrosi73 <andgass13@gmail.com>
2023-12-09 19:36:06 +0100

    s

--- get_youtube_video_list.py
+++ get_youtube_video_list.py
@@ -31,9 +31,6 @@ def get_video_list(channel_id, api_key, output_file):
         next_page_token = playlist_items.get('nextPageToken')
         if not next_page_token:
             break
-
-    # Write the extracted data to a JSON file
     with open(output_file, 'w') as json_file:
         json.dump(total_results, json_file, indent=2)
-
-    print(f'Data has been written to {output_file}')
+    print(f'video metadata has been downloaded to {output_file}')
